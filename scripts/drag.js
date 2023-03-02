5a44e07c123b2a874d3993e018264f7451deff8e
Saky-Dev <garciaisaac468@gmail.com>
2023-03-01 22:05:18 -0600

    Bug fixed when user creates an state; Now the transition name is showed with the arrow

--- scripts/drag.js
+++ scripts/drag.js
@@ -1,3 +1,16 @@
+const getTransitions = () => {
+  const init_state = link.starting.getAttribute('key')
+  const final_state = link.ending.getAttribute('key')
+
+  let transitions = []
+
+  Object.keys(states[init_state].transitions).forEach(key => {
+    if (states[init_state].transitions[key].has(final_state))
+      transitions.push(key)
+  })
+
+  return transitions
+}
 /* This functino do calculations with the measures of linked states
  * first get the position at center of state, then width alll the
  * positions calc the measures of svg element that connect the states
@@ -28,9 +41,10 @@ const statesVisualData = () => {
  * with the displacement, add the measures to svg element and finally
  * add the path into svg and svg to drag area */
 const drawDiagonalPath = name => {
-  const {vector, direction} = statesVisualData()
+  const {position, vector, direction} = statesVisualData()
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
   const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  const text = document.createElement('span')
 
   const displacement = 100 * 30 / (vector.h > vector.w ? vector.h : vector.w)
   const move = {
@@ -57,8 +71,14 @@ const drawDiagonalPath = name => {
 
   path.setAttributeNS(null, 'd', `M ${path_data.m} C ${path_data.c[0]}, ${path_data.c[1]}, ${path_data.c[2]}`)
 
+  text.className = 'indicator'
+  text.innerHTML = `${getTransitions().map(tr => tr)}`
+  text.style.top = `${position.sty + (direction.v === 'down' ? move.h : - move.h * 2)}px`
+  text.style.left = `${position.stx + (direction.h === 'right' ? move.w * 1.2 : -move.w * 2)}px`
+
   svg.appendChild(path)
   box_drag.appendChild(svg)
+  box_drag.appendChild(text)
 }
 /* Here the program can draw a linear line if one of the both coordinates
  * between the two states are the same, so it receives if the states are in
@@ -66,9 +86,10 @@ const drawDiagonalPath = name => {
  * to path, add that information and finally add both elements (svg, path )
  * to drag area */
 const drawLinearPath = (side, name) => {
-  const {vector, direction} = statesVisualData()
+  const {position, vector, direction} = statesVisualData()
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
   const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  const text = document.createElement('span')
 
   let path_data = undefined
   
@@ -93,8 +114,20 @@ const drawLinearPath = (side, name) => {
 
   path.setAttributeNS(null, 'd', `M ${path_data.m} L ${path_data.l}`)
 
+  text.className = 'indicator'
+  text.innerHTML = `${getTransitions().map(tr => tr)}`
+
+  if (side === 'horizontal') {
+    text.style.top = `${position.sty - 10}px`
+    text.style.left = `${direction.h === 'right' ? position.stx + 40 : position.stx - 80}px`
+  } else {
+    text.style.top = `${direction.v === 'down' ? position.sty + 40 : position.sty - 60}px`
+    text.style.left = `${position.stx - 10}px`
+  }
+
   svg.appendChild(path)
   box_drag.appendChild(svg)
+  box_drag.appendChild(text)
 }
 
 /* This function draw a semi circle that link the same state
@@ -104,6 +137,7 @@ const drawCyclePath = name => {
   const {position} = statesVisualData()
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
   const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  const text = document.createElement('span')
 
   svg.classList.add('link')
   svg.setAttribute('key', `${link.starting.getAttribute('key')}${name}${link.ending.getAttribute('key')}`)
@@ -116,8 +150,14 @@ const drawCyclePath = name => {
 
   path.setAttribute('d', `M 35 65 C 35 65, 65 65, 65 35 C 65 5, 35 5, 35 5 C 5 5, 5 30, 5 30`)
 
+  text.className = 'indicator'
+  text.innerHTML = `${getTransitions().map(tr => tr)}`
+  text.style.top = `${position.sty - 10}px`
+  text.style.left = `${position.stx + 35}px`
+
   svg.appendChild(path)
   box_drag.appendChild(svg)
+  box_drag.appendChild(text)
 }
 
 /* This is an object that saves functions linked to actions
@@ -132,12 +172,10 @@ const stateTools = {
    * or spae that is changed to lambda, then the program draw the link
    * and finally the link is added to states object and the link is clean */
   'handleLink': e => {
-    if (link.starting)
-      link.ending = e.target
-    else {
-      link.starting = e.target
-      return false
-    }
+    if (!link.starting)
+      return link.starting = e.target
+      
+    link.ending = e.target
     
     const {position} = statesVisualData()
     const init_state = link.starting.getAttribute('key')
@@ -151,33 +189,29 @@ const stateTools = {
       
       if (name.length > 1)
         name = undefined
-
       if (name.trim() === '')
         name = 'λ'
     }
 
+    if (!transitions[name])
+      transitions[name] = new Set()
+    if (!transitions[name].has(final_state))
+      transitions[name].add(final_state)
+    else
+      return alert('Esta transición ya existe')
+
     ;(() => {
       if (init_state === final_state)
         return drawCyclePath(name)
-
       if (position.stx === position.edx)
         return drawLinearPath('vertical', name)
-
       if (position.sty === position.edy)
         return drawLinearPath('horizontal', name)
 
       drawDiagonalPath(name)
     })()
 
-    link = { starting: undefined, ending: undefined }
-
-    if (!transitions[name])
-      transitions[name] = new Set()
-
-    if (!transitions[name].has(final_state))
-      return transitions[name].add(final_state)
-
-    alert('Esta transición ya existe')
+    link = {starting: undefined, ending: undefined}
   },
   /* Function to remove an state first of the states' object
    * and then fron thw UI */
@@ -245,8 +279,8 @@ const handleAdd = e => {
   node.draggable = true
   node.tabIndex = '-1'
 
-  node.innerText = name || `Q${state_num++}`
   node.setAttribute('key', name || `Q${state_num++}`)
+  node.innerText = node.getAttribute('key')
   node.addEventListener('dragstart', evt => action === 'Cursor' ? stateTools['handleDragstart'](evt) : false)
   node.addEventListener('dragend', evt => action === 'Cursor' ? stateTools['handleDragend'](evt) : false)
   node.addEventListener('contextmenu', evt => action === 'Cursor' ? stateTools['handleContextmenu'](evt) : false)
